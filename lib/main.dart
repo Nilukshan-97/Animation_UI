781bd7fb89c6e482363c5895e41ae58bc10528d9
Nilukshan-97 <nilukshanravi97@gmail.com>
2021-04-24 11:57:45 -0700

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -31,87 +31,179 @@ class _LoginPageState extends State<LoginPage> {
   int _pageState = 0;
   var _backgroundColor = Colors.white;
   var _headingColor = Color(0xFFB40284A);
+
+  double _headingTop = 100;
+
+  double _loginWidth = 0;
+  double _loginYOffset = 0;
+  double _loginXOffset = 0;
+  double _registerYOffset = 0;
+  double _loginOpacity = 1;
+
+  double windowWidth = 0;
+  double windowHeight = 0;
+
   @override
   Widget build(BuildContext context) {
+    windowHeight = MediaQuery.of(context).size.height;
+    windowWidth = MediaQuery.of(context).size.width;
+
     switch (_pageState) {
       case 0:
         _backgroundColor = Colors.white;
         _headingColor = Color(0xFFB40284A);
+        _loginYOffset = windowHeight;
+        _loginWidth = windowWidth;
+        _loginXOffset = 0;
+        _loginOpacity = 1;
+        _registerYOffset = windowHeight;
+
+        _headingTop = 100;
         break;
       case 1:
         _backgroundColor = Color(0xFFBD34C59);
         _headingColor = Colors.white;
+        _loginYOffset = 250;
+        _loginWidth = windowWidth;
+        _loginXOffset = 0;
+        _loginOpacity = 1;
+
+        _registerYOffset = windowHeight;
+
+        _headingTop = 90;
         break;
       case 2:
         _backgroundColor = Color(0xFFBD34C59);
         _headingColor = Colors.white;
+        _loginYOffset = 220;
+        _loginWidth = windowWidth - 40;
+        _loginXOffset = 20;
+        _loginOpacity = 0.7;
+        _registerYOffset = 250;
+
+        _headingTop = 80;
         break;
     }
-    return AnimatedContainer(
-      curve: Curves.fastLinearToSlowEaseIn,
-      duration: Duration(milliseconds: 1000),
-      color: _backgroundColor,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Container(
-            child: Column(
-              children: <Widget>[
-                Container(
-                    margin: const EdgeInsets.only(top: 100),
-                    child: Text(
-                      "Learn Free",
-                      style: TextStyle(
-                          color: _headingColor,
-                          fontSize: 28,
-                          fontWeight: FontWeight.bold),
-                    )),
-                Container(
-                  margin: EdgeInsets.all(25),
-                  padding: EdgeInsets.symmetric(horizontal: 32),
-                  child: Text(
-                    "The following defines the version and build number for your application.",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: _headingColor, fontSize: 16),
-                  ),
-                )
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 32),
-            child: Center(
-              child: Image.asset("assets/images/splash_bg.png"),
-            ),
-          ),
-          Container(
-            child: GestureDetector(
-              onTap: () {
-                setState(() {
-                  if (_pageState != 0) {
-                    _pageState = 0;
-                  } else {
-                    _pageState = 1;
-                  }
-                });
-              },
-              child: Container(
-                margin: EdgeInsets.all(32),
-                padding: EdgeInsets.all(20),
-                width: double.infinity,
-                decoration: BoxDecoration(
-                    color: Color(0xFFB40284A),
-                    borderRadius: BorderRadius.circular(50)),
-                child: Center(
-                    child: Text(
-                  "Get Started",
-                  style: TextStyle(color: Colors.white, fontSize: 16),
-                )),
+    return Stack(
+      children: <Widget>[
+        AnimatedContainer(
+          curve: Curves.fastLinearToSlowEaseIn,
+          duration: Duration(milliseconds: 1000),
+          color: _backgroundColor,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _pageState = 0;
+                        });
+                      },
+                      child: Container(
+                        child: Column(
+                          children: <Widget>[
+                            AnimatedContainer(
+                                curve: Curves.fastLinearToSlowEaseIn,
+                                duration: Duration(milliseconds: 1000),
+                                margin: EdgeInsets.only(top:_headingTop),
+                                child: Text(
+                                  "Learn Free",
+                                  style: TextStyle(
+                                      color: _headingColor,
+                                      fontSize: 28,
+                                      fontWeight: FontWeight.bold),
+                                )),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(25),
+                      padding: EdgeInsets.symmetric(horizontal: 32),
+                      child: Text(
+                        "The following defines the version and build number for your application.",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: _headingColor, fontSize: 16),
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
-      ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 32),
+                child: Center(
+                  child: Image.asset("assets/images/splash_bg.png"),
+                ),
+              ),
+              Container(
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      if (_pageState != 0) {
+                        _pageState = 0;
+                      } else {
+                        _pageState = 1;
+                      }
+                    });
+                  },
+                  child: Container(
+                    margin: EdgeInsets.all(32),
+                    padding: EdgeInsets.all(20),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Color(0xFFB40284A),
+                        borderRadius: BorderRadius.circular(50)),
+                    child: Center(
+                        child: Text(
+                      "Get Started",
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    )),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            setState(() {
+              _pageState = 2;
+            });
+          },
+          child: AnimatedContainer(
+              width: _loginWidth,
+              curve: Curves.fastLinearToSlowEaseIn,
+              duration: Duration(milliseconds: 1000),
+              transform:
+                  Matrix4.translationValues(_loginXOffset, _loginYOffset, 1),
+              decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(_loginOpacity),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(25),
+                    topRight: Radius.circular(25),
+                  ))),
+        ),
+        GestureDetector(
+          onTap: () {
+            setState(() {
+              _pageState = 1;
+            });
+          },
+          child: AnimatedContainer(
+              curve: Curves.fastLinearToSlowEaseIn,
+              duration: Duration(milliseconds: 1000),
+              transform: Matrix4.translationValues(0, _registerYOffset, 1),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(25),
+                    topRight: Radius.circular(25),
+                  ))),
+        )
+      ],
     );
   }
 }
